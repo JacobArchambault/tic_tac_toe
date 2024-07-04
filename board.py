ee473aad4089284b65733de4ae6bb58b345dec2c
Jacob Archambault <jacobarchambault@gmail.com>
2024-07-04 14:13:24 -0400

    replace 2d array game board with list, add victory_for function

--- board.py
+++ board.py
@@ -1,35 +1,46 @@
-def display_board(board):
+def display(board):
     # The function accepts one parameter containing the board's current status
     # and prints it out to the console.
-    for row in board:
-        print(f"+-------+-------+-------+\n|       |       |       |\n|   {row[0]}   |   {row[1]}   |   {row[2]}   |\n|       |       |       |")
-    print("+-------+-------+-------+")
+    print(f"""+-------+-------+-------+
+|       |       |       |
+|   {board[0]}   |   {board[1]}   |   {board[2]}   |
+|       |       |       |
++-------+-------+-------+
+|       |       |       |
+|   {board[3]}   |   {board[4]}   |   {board[5]}   |
+|       |       |       |
++-------+-------+-------+
+|       |       |       |
+|   {board[6]}   |   {board[7]}   |   {board[8]}   |
+|       |       |       |
++-------+-------+-------+""")
 
 
-board_dict = {1: (0, 0),
-2: (0,1), 
-3: (0,2),
-4: (1,0),
-5: "X",
-6: (1,2),
-7: (2,0),
-8: (2,1),
-9: (2,2)}
+winning_moves_dictionary = {
+    # pairs each index with the tuples required to complete a row with it 
+    0: ((1, 2), (3, 6), (4, 8)),
+    1: ((0, 2), (4, 7)),
+    2: ((0, 1), (5, 8), (4, 6)),
+    3: ((4, 5), (0, 6)),
+    4: ((3, 5), (1, 7), (0, 8), (2, 6)),
+    5: ((3, 4), (2, 8)),
+    6: ((7, 8), (0, 3), (2, 4)),
+    7: ((6, 8), (1, 4)),
+    8: ((6, 7), (2, 5), (0, 4))
+}
 
 def enter_move(board):
     # The function accepts the board's current status, asks the user about their move, 
     # checks the input, and updates the board according to the user's decision.
     while True:
         try:
-            move = int(input("Enter your move: "))
-            index = board_dict[move]
-            if board[index[0]][index[1]] not in ("O", "X"):
-                board[index[0]][index[1]] = "O"
-                board_dict[move] = "O"
-                return
+            index = int(input("Enter your move: ")) - 1
+            if board[index] not in ("O", "X"):
+                board[index] = "O"
+                return index, "O"
             else:
                 print("That space is already occupied")
-        except (ValueError, KeyError):
+        except (ValueError, IndexError):
             print("Invalid input. Please enter a number between 1 and 9")
 
 
@@ -37,12 +48,19 @@ def enter_move(board):
 def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares; 
     # the list consists of tuples, while each tuple is a pair of row and column numbers.
-    free = [x for x in board_dict.values() if x not in ("O", "X")]
+    return [x for x in board if x not in ("O", "X")]
 
 
-# def victory_for(board, sign):
-#     # The function analyzes the board's status in order to check if 
-#     # the player using 'O's or 'X's has won the game
+def victory_for(index, sign):
+    # The function analyzes the board's status in order to check if 
+    # the player using 'O's or 'X's has won the game
+    pairs_to_check = winning_moves_dictionary[index]
+    wins = False
+    for pair in pairs_to_check:
+        if pair[0] == pair[1] == sign: 
+            wins = True
+            break
+    return wins
 
 
 # def draw_move(board):
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,18 @@
-from board import display_board, enter_move, make_list_of_free_fields
+import board    
+game_board = [1,2,3,
+    4,"X",6,
+    7,8,9]
+board.display(game_board)
+index, sign = board.enter_move(game_board)
+wins = board.victory_for(index, sign)
+board.display(game_board)
+# available_moves = make_list_of_free_fields(game_board)
 
-game_board = [[1,2,3],
-    [4,"X",6],
-    [7,8,9]]
-display_board(game_board)
-enter_move(game_board)
-display_board(game_board)
-make_list_of_free_fields(game_board)
+# display board
+# prompt user to make first move
+# validate input
+    # error message, reprompt user
+# update board
+# check if there's a winner
+# computer move
+#
