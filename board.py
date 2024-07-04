1b5b8d686d5c4902c813d42bb6a52cfb95e07d2f
Jacob Archambault <jacobarchambault@gmail.com>
2024-07-04 15:44:12 -0400

    Move status functions to referee.py

--- board.py
+++ board.py
@@ -15,20 +15,6 @@ def display(board):
 |       |       |       |
 +-------+-------+-------+""")
 
-
-winning_moves_dictionary = {
-    # pairs each index with the tuples required to complete a row with it 
-    0: ((1, 2), (3, 6), (4, 8)),
-    1: ((0, 2), (4, 7)),
-    2: ((0, 1), (5, 8), (4, 6)),
-    3: ((4, 5), (0, 6)),
-    4: ((3, 5), (1, 7), (0, 8), (2, 6)),
-    5: ((3, 4), (2, 8)),
-    6: ((7, 8), (0, 3), (2, 4)),
-    7: ((6, 8), (1, 4)),
-    8: ((6, 7), (2, 5), (0, 4))
-}
-
 def enter_move(board):
     # The function accepts the board's current status, asks the user about their move, 
     # checks the input, and updates the board according to the user's decision.
@@ -43,13 +29,3 @@ def enter_move(board):
         except (ValueError, IndexError):
             print("Invalid input. Please enter a number between 1 and 9")
 
-def victory_for(index, sign, board):
-    # The function analyzes the board's status in order to check if 
-    # the player using 'O's or 'X's has won the game
-    pairs_to_check = winning_moves_dictionary[index]
-    wins = False
-    for pair in pairs_to_check:
-        if board[pair[0]] == board[pair[1]] == sign: 
-            wins = True
-            break
-    return wins, sign
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,6 @@
 import board    
 import computer_player
+import referee
 
 game_board = [1,2,3,
     4,"X",6,
@@ -7,14 +8,14 @@ game_board = [1,2,3,
 while True:
     board.display(game_board)
     index, sign = board.enter_move(game_board)
-    wins, sign = board.victory_for(index, sign, game_board)
+    wins, sign = referee.victory_for(index, sign, game_board)
     if wins:
         board.display(game_board)
         print(f"{sign} wins!")
         break
     board.display(game_board)
     index, sign = computer_player.draw_move(game_board)
-    wins, sign = board.victory_for(index, sign, game_board)
+    wins, sign = referee.victory_for(index, sign, game_board)
     if wins:
         board.display(game_board)
         print(f"{sign} wins!")
