8d63d37e8ae5d474eb10f8f1fe8cc0351b666340
Jacob Archambault <jacobarchambault@gmail.com>
2024-07-04 16:43:50 -0400

    update comment

--- computer_player.py
+++ computer_player.py
@@ -1,7 +1,8 @@
 from random import choice
 
 def draw_move(board):
-    # The function draws the computer's move and updates the board.
+    # The function performs the computer's move 
+    # and updates the board.
     index = choice([x for x in board if x not in ("O", "X")]) - 1
     board[index] = "X"
     return index, "X"
