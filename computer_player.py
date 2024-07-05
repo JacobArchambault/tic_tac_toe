5aa7580395dbe67bfa863ebe05c34a63c1071ff6
Jacob Archambault <jacobarchambault@gmail.com>
2024-07-04 20:56:29 -0400

    Update so that computer doesn't always choose middle spot on first move

--- computer_player.py
+++ computer_player.py
@@ -1,6 +1,6 @@
 from random import choice
 
-def draw_move(board):
+def enter_move(board):
     # The function performs the computer's move 
     # and updates the board.
     index = choice([x for x in board if x not in ("O", "X")]) - 1
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,12 +4,12 @@ import computer_player
 import referee
 
 game_board = [1,2,3,
-    4,"X",6,
+    4,5,6,
     7,8,9]
 turn = 1
-while turn <= 8:
+while turn <= 9:
     console_board.display(game_board)
-    index, sign = console_player.enter_move(game_board) if turn % 2 else computer_player.draw_move(game_board)
+    index, sign = computer_player.enter_move(game_board) if turn % 2 else console_player.enter_move(game_board)
     referee.victory_for(index, sign, game_board)
     turn += 1
 console_board.display(game_board)
